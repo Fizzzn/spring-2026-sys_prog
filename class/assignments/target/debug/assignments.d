d954af4b893ecc245cee1b5b2249d9ef2288132c
Fizzzn <btordz6@gmail.com>
2026-02-06 23:45:43 +0000

    Submit assignments homework

---
+++ class/assignments/target/debug/assignments.d
@@ -0,0 +1,1 @@
+/workspaces/spring-2026-sys_prog/class/assignments/target/debug/assignments: /workspaces/spring-2026-sys_prog/class/assignments/src/main.rs
---
+++ class/assignments/target/debug/deps/assignments-707cbec3b6a32f02.d
@@ -0,0 +1,5 @@
+/workspaces/spring-2026-sys_prog/class/assignments/target/debug/deps/assignments-707cbec3b6a32f02.d: src/main.rs
+
+/workspaces/spring-2026-sys_prog/class/assignments/target/debug/deps/assignments-707cbec3b6a32f02: src/main.rs
+
+src/main.rs:
